d0fd1e9a214d64853fc03c93611045f3dca430d4
qsd <salutrr>
2017-10-31 11:07:24 +0100

    update*

--- prog/smtp/script.sh
+++ prog/smtp/script.sh
@@ -1,17 +1,18 @@
-sudo apt-get --assume-yes install postfix postfix-mysql libsasl2-modules sasl2-bin courier-imap
-sudo groupadd -g 5000 vmail
-sudo useradd –g vmail –u 5000 vmail –d /home/vmail
-sudo mkdir /var/mail/
-sudo chown -R vmail:vmail /var/mail
-sudo cp master.cf /etc/postfix
-sudo cp main.cf /etc/postfix
-sudo touch /etc/postfix/vmailbox
-sudo echo "michael@entreprise michael/" >> /etc/postfix/vmailbox
-sudo maildirmake /var/mail/michael
-chown -R vmail:vmail /var/mail/$1
+apt-get --assume-yes install postfix courier-imap courier-authdaemon
+groupadd vmail -g 5000
+useradd –g vmail –u 5000 vmail –d /home/vmail
+mkdir /home/vmail
+mkdir /var/mail/
+chown -R vmail:vmail /var/mail
+cp master.cf /etc/postfix
+cp main.cf /etc/postfix
+touch /etc/postfix/vmailbox
+echo "michael@entreprise michael/" >> /etc/postfix/vmailbox
+maildirmake /var/mail/michael
+chown -R vmail:vmail /var/mail/michael
 userdb michael set uid=5000 gid=5000 home=/var/mail/michael mail=/var/mail/michael
 userdb michael set systempw=$(openssl passwd -1 michael)
-sed -i.bak -E 's/^([ \t]*authmodulelist[ \t]*=[ \t]*).*/\1'"authuserdb"'/' /etc/courier/authdaemon
+sed -i.bak -E 's/^([ \t]*authmodulelist[ \t]*=[ \t]*).*/\1'"authuserdb"'/' /etc/courier/authdaemonrc
 /etc/init.d/courier-authdaemon restart
 postmap /etc/postfix/vmailbox
 service postfix restart
