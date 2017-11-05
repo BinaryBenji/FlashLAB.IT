5eca63ec268e3ae9087af31e031966402e21d370
BinaryBenji <ben639dx@gmail.com>
2017-11-05 16:42:21 +0100

    Push cli-ser, reorder dirs

---
+++ cli-ser/ser/routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var PythonShell = require('python-shell');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.post('/', function(req, res, next) {
+  key = req.body.key
+  pass = '$2a$10$33USXSTL1p7WuPIah5TwX.shP3z6mRymUqFv1BOZgXm.pJhzoblsi'
+
+  if (key === pass){
+  	res.render('success');
+  	console.log('Infrastructure deployment requested.')
+  	/* WARNING : Set path to the script */ 
+
+ //  	var options = {
+	//   scriptPath: '../',
+	// };
+
+  	PythonShell.run('launch.py', function (err, results) {
+  		if (err) throw err;
+		console.log('%j', results);
+	});
+  }
+  else{
+  	res.render('failure');
+  	console.log('Someone is trying to bruteforce the launch form.')
+  }
+});
+
+module.exports = router;
