2fbc8e6bdf8c18fee6e641810b2cf6628aeee3e6
Benjamin <ben639dx@gmail.com>
2017-11-12 18:22:39 +0100

    changes suid

--- prog/suid/deploy.sh
+++ prog/suid/deploy.sh
@@ -1,28 +1,19 @@
-#!/bin/bash
-
 cd /home/ubuntu
 echo "
+#include <stdio.h>
 #include <stdlib.h>
-    #include <stdio.h>
-     
-    int main(void)
-    {
-            system("ls /home/ubuntu/.passwd");
-            return 0;
-    }
-" > key.c
 
+int main(void)
+{
+	system(\"ls /home/ubuntu/.passwd\");
+	return (0);
+}" > key.c
 gcc key.c
-echo "fs6d7fsd41vcx4vxcvdfsgf" > .passwd
-
-passwd ubuntu << EOF
-admin10
-admin10
-
+echo "f4ds67f6dsq1f34sdq314fsd" > .passwd
 chmod 440 key.c
 chmod 4550 a.out
 chmod 440 .passwd
-
 chown ubuntu:ubuntu key.c
 chown root:root .passwd
 chown root:ubuntu a.out
+
