4f718eaf430147a371330d3327193e9935350a11
root <root@flashlab.itinet.fr>
2017-11-14 16:01:06 +0100

    oiudsjf

--- prog/dnsftpsmtp/accounts/script.sh
+++ prog/dnsftpsmtp/accounts/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#CREATION USER MACHINE POSEIDON
+CREATION USER MACHINE POSEIDON
 useradd poseidon -d /home/poseidon -m
 passwd poseidon <<EOF
 dieudelamer
 dieudelamer
 EOF
 
-#CREATION USER FTP
+CREATION USER FTP
 adduser zeus <<EOF
 admin10
 admin10
