5fd5c2350ca9e5dd718c21cb7e7c68ac666f18c3
root <root@flashlab.itinet.fr>
2017-11-17 12:13:50 +0100

    Finishing indices

--- prog/dnsftpsmtp/indices/script.sh
+++ prog/dnsftpsmtp/indices/script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-touch /home/poseidon/indices
-echo "Pas d'indices pour l'instant..." > /home/poseidon/indices
+mkdir /home/poseidon/indices
+touch /home/poseidon/indices/sorry
+echo "Pas d'indices pour l'instant..." > /home/poseidon/indices/sorry
 touch /var/tmp/indice1.sh
-echo "echo \"Indice 1) Vous avez un mail non lu\" > /home/poseidon/indices" > /var/tmp/indice1.sh
+echo "echo \"Indice 1) Vous avez un mail non lu\" > /home/poseidon/indices/indice1" > /var/tmp/indice1.sh
 touch /var/tmp/indice2.sh
-echo "echo \"Indice 2) CourrierIMAP ça peut être utile\" >> /home/poseidon/indices" > /var/tmp/indice2.sh
-crontab -l > indices/crontab
-crontab indices/crontab
-rm indices/crontab
+echo "echo \"Indice 2) CourrierIMAP ça peut être utile\" >> /home/poseidon/indices/indice2" > /var/tmp/indice2.sh
+crontab -l > crontab
+echo "*/5 * * * * bash /var/tmp/indice1.sh" > crontab
+echo "*/10 * * * * bash /var/tmp/indice2.sh" >> crontab
+crontab crontab
+rm crontab
