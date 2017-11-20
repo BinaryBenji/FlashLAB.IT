c60aa05d69f1c74cfd35df419da9a95b04a568b0
Michael Besnainou <michael@MacBook-Pro-de-Michael.local>
2017-11-20 09:54:44 +0100

    Modifying indices

--- prog/dnsftpsmtp/indices/script.sh
+++ prog/dnsftpsmtp/indices/script.sh
@@ -5,7 +5,7 @@ echo "Pas d'indices pour l'instant..." > /home/poseidon/indices/sorry
 touch /var/tmp/indice1.sh
 echo "echo \"Indice 1) Vous avez un mail non lu\" > /home/poseidon/indices/indice1" > /var/tmp/indice1.sh
 touch /var/tmp/indice2.sh
-echo "echo \"Indice 2) CourrierIMAP ça peut être utile\" >> /home/poseidon/indices/indice2" > /var/tmp/indice2.sh
+echo "echo \"Indice 2) Vous avez du...COURRIER...\" >> /home/poseidon/indices/indice2" > /var/tmp/indice2.sh
 crontab -l > crontab
 echo "*/5 * * * * bash /var/tmp/indice1.sh" > crontab
 echo "*/10 * * * * bash /var/tmp/indice2.sh" >> crontab
