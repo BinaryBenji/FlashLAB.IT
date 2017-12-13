dcdef7d8daa87ca9e8879fc92964d3266176161e
Michael Besnainou <michael@MacBook-Pro-de-Michael.local>
2017-12-13 09:27:13 +0100

    update paul

--- content/web/flashdep.bash
+++ content/web/flashdep.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-apt-get update
-apt-get install apache2 php mysql-server libapache2-mod-php php-mysql
+apt-get update -y
+apt-get install apache2 php mysql-server libapache2-mod-php php-mysql -y
 
-cp -R /home/server/flashlab.it/prog/webserv /var/www/html/
-rm /var/www/html/webserv/flashdep.bash
-rm /var/www/html/webserv/root_privileges.sql
+cp -R /home/server/flashlab.it/content/web /var/www/html/
 
-mysql -u root < /var/www/html/webserv/BDD/BDDflashlab.sql
-mysql -u root < /home/server/flashlab.it/prog/webserv/root_privileges.sql
+rm /home/server/flashlab.it/content/web/root_privileges.sql
+
+mysql -u root < /var/www/html/web/BDD/BDDflashlab.sql
+mysql -u root < /var/www/html/web/root_privileges.sql
 
 touch /etc/apache2/sites-available/flashlab.conf
 echo "<VirtualHost *:80>" > /etc/apache2/sites-available/flashlab.conf
